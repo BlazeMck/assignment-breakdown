91e924e2d100dd0084e3c0fb71cac1aa4a8e7e4a
Riley McKinlay <blazemckinlay@gmail.com>
2026-06-10 23:46:55 -0600

    Move the className prop addition to the end of the Button's class so new classes can override previous ones.

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -10,7 +10,7 @@ export default function Button({
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={className + " rounded-md bg-blue-500 text-white p-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 disabled:cursor-not-allowed"}
+            className={"rounded-md bg-blue-500 text-white p-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 disabled:cursor-not-allowed " + className}
         >
             {children}
         </button>
